ef8d1b78d8369be2e8aa54b8d613b2757e9700a3
pepesan <pepesan@gmail.com>
2019-12-18 18:57:55 +0100

    metido ejemplo de actionsheetcontroller

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { ActionSheetController } from '@ionic/angular';
 import {ModalPage} from '../modal/modal';
 @Component({
   selector: 'app-home',
@@ -8,7 +9,9 @@ import {ModalPage} from '../modal/modal';
 })
 export class HomePage {
 
-  constructor(public modalController: ModalController) {}
+  constructor(
+      public modalController: ModalController,
+      public actionSheetController: ActionSheetController) {}
 
   async presentModal() {
     const modal = await this.modalController.create({
@@ -25,4 +28,44 @@ export class HomePage {
     error(error) {
         console.log('error de carga de imagen' + error);
     }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'arrow-dropright-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
